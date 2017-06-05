14381ce88da0a5dcf43900cb74dca850a6232160
xulilsjr <xuli5@le.com>
2017-06-05 14:56:44 +0800

    no message

---
+++ WeiXinProject/WeiXinProject/Classes/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  WeiXinProject
+//
+//  Created by 徐丽 on 2017/6/5.
+//  Copyright © 2017年 徐丽. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ WeiXinProject/WeiXinProject/Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  WeiXinProject
+//
+//  Created by 徐丽 on 2017/6/5.
+//  Copyright © 2017年 徐丽. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
